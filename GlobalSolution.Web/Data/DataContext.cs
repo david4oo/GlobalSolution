d606fb4a87c7d5719952656c64aa904dbb515a22
David <walteraraujo206571@correo.itm.edu.co>
2019-11-13 06:16:03 -0500

    Init Project

---
+++ GlobalSolution.Web/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using GlobalSolution.Web.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GlobalSolution.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
+
